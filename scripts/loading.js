11a1325a75d988b220c98ba21807461b91f7e25a
Michael Li <fakebear@Nuclear-Toaster.local>
2020-10-14 16:23:03 -0400

    changed loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,1 +1,21 @@
-//runs first when user enters page
+window.onload = loadAnimations();
+
+function loadAnimations(){
+  console.log("I don't understand.");
+}
+
+var charlst = ['!','<','>','-','_','/','[',']','{','}','=','*','^','?','#'];
+function scrambleText(object, str){
+  var obj = Document.querySelector(object);
+  
+  //scrambling algorithm
+  function scramble(obj,str){
+    if(str.length==0)return;
+    for(let i = 0; i<3; i++){
+      obj.innerHTML = obj.innerHTML + charlst[Math.floor(Math.random() * charlst.length) + 1];
+    }
+  }
+}
+
+
+
