87eddd738b9b01ccc9328eff9476deedb0e69909
Michael Li <fakebear@Nuclear-Toaster.local>
2020-10-15 00:24:13 -0400

    added fade-in and text scramble transitions to home

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,21 +1,50 @@
+var charlst = ['!','-','_','/','[',']','{','}','=','*','^','?','#'];
+var random = 3;
 window.onload = loadAnimations();
 
 function loadAnimations(){
-  console.log("I don't understand.");
+  fadein("#me",0.95,0.04);
+  scrambleText("#sofdev","Software Developer");
+  setTimeout(() => scrambleText("#name","MICHAEL LI"),500);
+  setTimeout(() => fadein("#bio",0.5,0.05),500);
+  setTimeout(() => fadein("#resume-link",1.0,0.05),700);
 }
 
-var charlst = ['!','<','>','-','_','/','[',']','{','}','=','*','^','?','#'];
-function scrambleText(object, str){
-  var obj = Document.querySelector(object);
-  
-  //scrambling algorithm
-  function scramble(obj,str){
+function scrambleText(object,str){
+  var obj = document.querySelector(object);
+  obj.innerHTML = "";
+  function assemble(str){
     if(str.length==0)return;
-    for(let i = 0; i<3; i++){
-      obj.innerHTML = obj.innerHTML + charlst[Math.floor(Math.random() * charlst.length) + 1];
-    }
+    let count = 0;
+    obj.innerHTML = obj.innerHTML + "_";
+    var scram = setInterval(() => {
+      let randomChar = charlst[Math.floor(Math.random()*13)];
+      obj.innerHTML = obj.innerHTML.substring(0,obj.innerHTML.length-1)
+                    + randomChar; 
+      if(count==3){
+        obj.innerHTML = obj.innerHTML.substring(0,obj.innerHTML.length-1) + str.charAt(0);
+        assemble(str.substring(1,str.length));
+        clearInterval(scram);
+      }
+      count++;
+    }, 20);
   }
+  assemble(str);
 }
 
+function fadein(object,targetOpacity,rate){
+  var obj = document.querySelector(object);
+  let count = 0;
+  var fade = setInterval(() => {
+    if(obj.style.opacity>=targetOpacity)clearInterval(fade);
+    count+=rate;
+    obj.style.opacity = count;
+  }, 20);
+}
 
+function zoom(object,dest,rate){
+  var obj = document.querySelector(object);
+  var move = setInterval(() => {
 
+  }, 20);
+}
