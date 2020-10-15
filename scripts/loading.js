4803d47ace6d27f1bb00d356def8d185d6308c1b
pseudobear <2002michaelli@gmail.com>
2020-10-15 06:26:53 +0000

    finished animations for home

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,10 +1,11 @@
 var charlst = ['!','-','_','/','[',']','{','}','=','*','^','?','#'];
-var random = 3;
 window.onload = loadAnimations();
 
 function loadAnimations(){
   fadein("#me",0.95,0.04);
   scrambleText("#sofdev","Software Developer");
+  zoomLeft("#nav-experiences",0,5);
+  zoomRight("#nav-about",0,5);
   setTimeout(() => scrambleText("#name","MICHAEL LI"),500);
   setTimeout(() => fadein("#bio",0.5,0.05),500);
   setTimeout(() => fadein("#resume-link",1.0,0.05),700);
@@ -14,14 +15,14 @@ function scrambleText(object,str){
   var obj = document.querySelector(object);
   obj.innerHTML = "";
   function assemble(str){
-    if(str.length==0)return;
+    if(str.length == 0)return;
     let count = 0;
     obj.innerHTML = obj.innerHTML + "_";
     var scram = setInterval(() => {
       let randomChar = charlst[Math.floor(Math.random()*13)];
       obj.innerHTML = obj.innerHTML.substring(0,obj.innerHTML.length-1)
                     + randomChar; 
-      if(count==3){
+      if(count == 3){
         obj.innerHTML = obj.innerHTML.substring(0,obj.innerHTML.length-1) + str.charAt(0);
         assemble(str.substring(1,str.length));
         clearInterval(scram);
@@ -36,15 +37,42 @@ function fadein(object,targetOpacity,rate){
   var obj = document.querySelector(object);
   let count = 0;
   var fade = setInterval(() => {
-    if(obj.style.opacity>=targetOpacity)clearInterval(fade);
-    count+=rate;
+    if(obj.style.opacity >= targetOpacity)clearInterval(fade);
+    count += rate;
     obj.style.opacity = count;
   }, 20);
 }
 
-function zoom(object,dest,rate){
+function zoomLeft(object,dest,rate){ //*** REQUIRES LEFT PROPERTY AND NO RIGHT
   var obj = document.querySelector(object);
+  obj.style.left = "2000px";
   var move = setInterval(() => {
+    let left = parseFloat(obj.style.left);
+    let dist = left-dest;
+    if(dist <= 10){
+      obj.style.left = dest.toString()+"px";
+      clearInterval(move);
+      return;
+    }
+    let diff = dist/rate;
+    left -= diff;
+    obj.style.left = left.toString()+"px";
+  }, 20);
+}
 
+function zoomRight(object,dest,rate){ //*** REQUIRES RIGHT PROPERTY AND NO LEFT
+  var obj = document.querySelector(object);
+  obj.style.right = "2000px";
+  var move = setInterval(() => {
+    let right = parseFloat(obj.style.right);
+    let dist = right-dest;
+    if(dist <= 0){
+      obj.style.right = dest.toString()+"px";
+      clearInterval(move);
+      return;
+    }
+    let diff = dist/rate;
+    right -= diff;
+    obj.style.right = right.toString()+"px";
   }, 20);
 }
